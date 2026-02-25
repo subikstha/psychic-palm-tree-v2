14cabc099e9ec3f8febf398aa0bf522e8e7f8069
subikstha <subsatch@gmail.com>
2026-02-25 16:28:20 +0545

    Added product detail page

--- lib/actions/product.action.ts
+++ lib/actions/product.action.ts
@@ -4,6 +4,7 @@ import db from "@/db";
 import { product } from "@/db/schema";
 import { LATEST_PRODUCTS_LIMIT } from "../constants";
 import { convertToPlainObject } from "../utils";
+import { eq } from "drizzle-orm";
 
 export async function getLatestProducts() {
   try {
@@ -16,5 +17,17 @@ export async function getLatestProducts() {
     return convertToPlainObject(data);
   } catch (error) {
     console.error("error", error);
+  }
+}
+
+export async function getProductBySlug(slug: string) {
+  try {
+    const [data] = await db
+      .select()
+      .from(product)
+      .where(eq(product.slug, slug));
+    return convertToPlainObject(data);
+  } catch (error) {
+    console.error("Error getting product by slug", error);
   }
 }
